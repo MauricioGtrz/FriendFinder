e45474e2411362601ebc3a413e16eb023fc5bf67
Mau <mau@pop-os.localdomain>
2020-09-25 01:12:55 -0700

    Added styling to project

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,46 @@
-var friendMatch = [
-    {
-      name: "Aaron Rodgers",
-      photo: "http://static3.businessinsider.com/image/597a40094528e62c008b4ec6/aaron-rodgers-is-structuring-his-life-like-tom-brady-in-order-to-play-football-into-his-40s.jpg ",
-      scores: ["2", "2", "2", "2", "2"]
-    }, {
-      name: "Aaron Judge",
-      photo: "http://image.nj.com/home/njo-media/width620/img/yankees_main/photo/mlb-baltimore-orioles-at-new-york-yankees-fa5cf650079734ba.jpg ",
-      scores: ["3", "3", "3", "3", "3"]
-    }, {
-      name: " Alex Morgan",
-      photo: "https://i.ytimg.com/vi/LH8RJ9Q0X0M/maxresdefault.jpg ",
-      scores: ["1", "1", "1", "1", "1"]
-    }, {
-      name: "Christen Press",
-      photo: "http://cdn.shopify.com/s/files/1/0661/5963/files/0010_christen_press_large.jpg?9601849381797396010 ",
-      scores: ["3", "3", "3", "3", "1"]
-    },
-  
-  ];
-  
-  
-  
-  module.exports = friendMatch;
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+var friends = [
+  {
+    name: "SpongeBob",
+    photo:
+      "https://vignette.wikia.nocookie.net/spongebob/images/d/d7/SpongeBob_stock_art.png/revision/latest?cb=20190921125147",
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"],
+  },
+  {
+    name: "Squidward",
+    photo:
+      "https://vignette.wikia.nocookie.net/spongebob/images/0/05/Squidward_stock_art.png/revision/latest?cb=20190921125123",
+    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"],
+  },
+  {
+    name: "Plankton",
+    photo:
+      "https://vignette.wikia.nocookie.net/spongebob/images/7/77/Plankton_stock_art.png/revision/latest?cb=20181202012432",
+    scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"],
+  },
+  {
+    name: "Sandy Cheeks",
+    photo:
+      "https://vignette.wikia.nocookie.net/spongebob/images/8/84/Sandy_stock_art.png/revision/latest?cb=20200407131514",
+    scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"],
+  },
+  {
+    name: "Patrick",
+    photo:
+      "https://vignette.wikia.nocookie.net/spongebob/images/5/5d/Patrick_stock_art.png/revision/latest?cb=20181202012306",
+    scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"],
+  },
+  {
+    name: "Gary",
+    photo:
+      "https://vignette.wikia.nocookie.net/spongebob/images/9/9e/Gary_looking_up_stock_art.png/revision/latest?cb=20181227162221",
+    scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"],
+  },
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
